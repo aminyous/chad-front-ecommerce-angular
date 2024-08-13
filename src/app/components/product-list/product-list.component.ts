e7964baec611b3ed040db33414d9d638e5bcbb84
Amine Hichri <amyn.hichri@gmail.com>
2024-08-13 17:40:01 +0200

    Last update V1

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Product } from 'src/app/common/product';
+import { ProductService } from 'src/app/services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list-table.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products: Product[] = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.productService.getProductList().subscribe((data) => {
+      this.products = data;
+      console.log(data);
+    });
+  }
+}
