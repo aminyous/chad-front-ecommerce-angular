1d71e500a91ad38bec290bb98c2ada4db09c70e8
Amine Hichri <amyn.hichri@gmail.com>
2024-08-17 10:56:27 +0200

    Add Search for products by keyword

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -5,44 +5,50 @@ import { Product } from '../common/product';
 import { ProductCategory } from '../common/product-category';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-  
-
   private baseURL = 'http://localhost:8082/api/products';
   private categoryURL = 'http://localhost:8082/api/product-category';
 
-  constructor(private httpClient: HttpClient) { }
+  constructor(private httpClient: HttpClient) {}
 
   getProductList(id: number): Observable<Product[]> {
+    const searchURL = `${this.baseURL}/search/findByCategoryId?id=${id}`;
 
-    const searchURL = `${this.baseURL}/search`;
-
-    return this.httpClient.get<GetResponseProduct>(`${searchURL}/findByCategoryId?id=${id}`)
-    .pipe(map(response => response._embedded.products));
+    return this.getProducts(searchURL);
   }
 
+  searchProducts(theKeyword: string): Observable<Product[]> {
+    const searchURL = `${this.baseURL}/search/findByNameContaining?name=${theKeyword}`;
+
+    return this.getProducts(searchURL);
+  }
+
+  private getProducts(searchURL: string): Observable<Product[]> {
+    return this.httpClient
+      .get<GetResponseProduct>(`${searchURL}`)
+      .pipe(map((response) => response._embedded.products));
+  }
+
+  
   getProductCategoriesList(): Observable<ProductCategory[]> {
-    
-
-    return this.httpClient.get<GetResponseProductCategory>(`${this.categoryURL}`)
-    .pipe(map(response => response._embedded.productCategory));
+    return this.httpClient
+      .get<GetResponseProductCategory>(`${this.categoryURL}`)
+      .pipe(map((response) => response._embedded.productCategory));
   }
 
+  
 }
-
-
 
 interface GetResponseProduct {
   _embedded: {
-    products : Product[]
-  }
+    products: Product[];
+  };
 }
-
 
 interface GetResponseProductCategory {
   _embedded: {
-    productCategory : ProductCategory[]
-  }
+    productCategory: ProductCategory[];
+  };
 }
